2be0f8728c698fdc99be1aaadb41158fd6815169
[Ignasi Muñoz Gol] <ignasimgol@gmail.com>
2024-12-29 14:35:39 +0400

    tailwind working

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/**/*.{html,js,scss}', // Asegúrate de incluir tus rutas
+      './views/**/*.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
